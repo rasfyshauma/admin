d2b98391c934fb695e9178ab82d9651bd564239b
rasfyshauma <55040597+rasfyshauma@users.noreply.github.com>
2021-04-29 13:48:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>Admin - Login </title>
+
+  <link rel="stylesheet" href="css/reset.css">
+
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+
+</head>
+
+<body>
+
+  <div class="wrap">
+		<div class="avatar">
+      <img src="http://cdn.ialireza.me/avatar.png">
+		</div>
+		<input type="text" placeholder="Username" required>
+		<div class="bar">
+			<i></i>
+		</div>
+		<input type="password" placeholder="Password" required>
+		<a href="" class="forgot_link">Lupa?</a>
+		<button>Sign in</button>
+	</div>
+
+  <script src="js/index.js"></script>
+
+</body>
+
+</html>
